f39a73da324ab1df3cb6ede1758f2b8cf2eb0585
Xinjie_Duan <duanxinjie119@gmail.com>
2013-02-10 14:05:26 +0200

    Login view

--- albumbr/albumbr/views.py
+++ albumbr/albumbr/views.py
@@ -2,6 +2,22 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib import auth
 
 def index(request):
 	return render_to_response('welcome/index.html', {})
+
+
+
+def login_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Correct password, and the user is marked "active"
+        auth.login(request, user)
+        # Redirect to a success page.
+        return HttpResponseRedirect("/account/loggedin/")
+    else:
+        # Show an error page
+        return HttpResponseRedirect("/account/invalid/")
