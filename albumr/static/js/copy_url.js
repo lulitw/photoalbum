04c2b8a1f787d7a53df648242f1bca62a58539a8
Xinjie_Duan <duanxinjie119@gmail.com>
2013-03-12 16:14:03 +0200

    Fixed "copy url" button and Social Media Share

--- albumr/static/js/copy_url.js
+++ albumr/static/js/copy_url.js
@@ -1,6 +1,19 @@
- function copyToClipBoard() {
-        var clipBoardContent = "";
-        clipBoardContent += this.location.href;
-        window.clipboardData.setData("Text", clipBoardContent);
-        alert("Sucess!");
-    }
+function copyToClipboard() {
+    var txt = ""
+    
+    txt += this.location.href;
+
+    ZeroClipboard.setMoviePath("http://www.track2web.com/wp-includes/js/zeroclipboard/ZeroClipboard.swf");
+
+    clip = new ZeroClipboard.Client();
+
+    clip.setHandCursor(true);
+
+    clip.setText(txt);
+
+    clip.addEventListener("complete", function (client, text) {
+        alert("URL Copied! \n" + text);
+    });
+
+    clip.glue("copy_url_button");
+}
